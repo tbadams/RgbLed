bae94d0a6637fa484a5bdc8b8c09da2539812a01
Trevor Adams <trevor@striiv.com>
2017-07-18 21:41:12 -0700

    Delete commented/borrowed code.

--- rgbled.py
+++ rgbled.py
@@ -24,55 +24,6 @@ RED.start(50)
 GREEN.start(50)
 BLUE.start(50)
 
-# try:
-
-    #     while RUNNING:
-#     #lighting up the pins. 100 means giving 100% to the pin
-#
-#     RED.start(100)
-#
-#     GREEN.start(1)
-#
-#     BLUE.start(1)
-#
-#     #For anode RGB LED users, if you want to start with RED too the only thing to be done is defining RED as one and GREEN and BLUE as 100.
-#
-#     for x in range(1,101):
-#
-#     #for changing the width of PWM, this command is used
-#
-#     GREEN.ChangeDutyCycle(x)
-#
-#     #for anode LED users, just change x with 101-x
-#
-#     #and for delay time.sleep is used. You can chance the duration of the colors with changing the time from here
-#
-#     time.sleep(0.05)
-#
-#     for x in range(1,101):
-#
-#     RED.ChangeDutyCycle(101-x)
-#
-#     time.sleep(0.025)
-#
-#     for x in range(1,101)
-#
-#     GREEN.ChangeDutyCycle(101-x)
-#
-#     BLUE.ChangeDutyCycle(x)
-#
-#     time.sleep(0.025)
-#
-#     for x in range(1,101):
-#
-#     RED.ChangeDutyCycle(x)
-#
-#     time.sleep(0.025)
-#
-# except KeyboardInterrupt:
-#     RUNNING = False
-#     GPIO.cleanup()
-
 
 def hsv(h, s, v):
     normalized_hsv = list(colorsys.hsv_to_rgb(h, s, v))
