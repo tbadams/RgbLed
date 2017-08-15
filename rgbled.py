b9d1d46b54eea009125b0b1651ffca25a5fd1d61
Trevor Adams <trevor@striiv.com>
2017-08-14 20:36:47 -0700

    Switch implementation to the 'pigpio' which uses a hardware clock and thus doesn't flicker like RPi.GPIO.

--- rgbled.py
+++ rgbled.py
@@ -1,57 +1,65 @@
-import RPi.GPIO as GPIO
 import time
 import colorsys
+import pigpio
 
-# http://www.instructables.com/id/Raspberry-Pi-3-RGB-LED-With-Using-PWM/
-
-GPIO.setmode(GPIO.BCM)
-RUNNING = True
-
-# Setup pins
-green = 20
-red = 16
-blue = 21
-GPIO.setup(red, GPIO.OUT)
-GPIO.setup(green, GPIO.OUT)
-GPIO.setup(blue, GPIO.OUT)
-
-#Configure PWM
-Freq = 100
-RED = GPIO.PWM(red, Freq)
-GREEN = GPIO.PWM(green, Freq)
-BLUE = GPIO.PWM(blue, Freq)
-RED.start(50)
-GREEN.start(50)
-BLUE.start(50)
+PIGPIO_MAX = 255.0
 
 
-def hsv(h, s, v):
-    normalized_hsv = list(colorsys.hsv_to_rgb(h, s, v))
-    normalized_hsv = tuple(int(i * 100) for i in normalized_hsv)
-    return rgb(*list(normalized_hsv))
+class RgbLed(object):
+    def __init__(self, red_gpio=16, green_gpio=20, blue_gpio=21, max_val=1.0):
+        self.pi = pigpio.pi()
+        self.red = red_gpio
+        self.green = green_gpio
+        self.blue = blue_gpio
+        self.max = max_val
 
+    def __del__(self):
+        self.pi.stop()
 
-def rgb(r, g, b):
-    RED.ChangeDutyCycle(100 - r)
-    GREEN.ChangeDutyCycle(100 - g)
-    BLUE.ChangeDutyCycle(100 - b)
+    def r(self, value):
+        """Set the red LED level to the specified value."""
+        self.pi.set_PWM_dutycycle(self.red, value * (PIGPIO_MAX / self.max))
 
+    def g(self, value):
+        """Set the green LED level to the specified value."""
+        self.pi.set_PWM_dutycycle(self.green, value * (PIGPIO_MAX / self.max))
 
-def hue_rotate(period = 4):
-    RUNNING = True
-    try:
-        while RUNNING:
-            for x in range(0, 1000):
-                hsv(x/1000.0, 1, 1)
-                time.sleep((period * 1.0)/1000)
-    except KeyboardInterrupt:
-        RUNNING = False
-        GPIO.cleanup()
+    def b(self, value):
+        """Set the blue LED level to the specified value."""
+        self.pi.set_PWM_dutycycle(self.blue, value * (PIGPIO_MAX / self.max))
 
+    def rgb(self, r, g, b):
+        """Set the RGB LED to the RGB color specified."""
+        self.r(r)
+        self.g(g)
+        self.b(b)
 
-def hue_transit(start = 0, end = 1.0, duration = 4):
-    diff = end - start
-    for x in range(0, 1000):
-        hsv(start + (x / 1000.0) * diff, 1, 1)
-        time.sleep(duration/1000.0)
+    def w(self, value):
+        """Set the LED to white with a brightness specified by the given value"""
+        self.rgb(value, value, value)
+
+    def hsv(self, h, s=1.0, v=1.0):
+        """Sets the LED to the color specified by the hue, saturation and value parameters.
+        's' and 'v' must be between 0 and 1, while h will be interpreted as a value between 0 and self.max."""
+        h /= self.max  # Normalize
+        normalized_rgb = list(colorsys.hsv_to_rgb(h, s, v))
+        in_rgb = [i * self.max for i in normalized_rgb]
+        self.rgb(*list(in_rgb))
+
+    def hue_rotate(self, period=4):
+        RUNNING = True
+        try:
+            while RUNNING:
+                for x in range(0, 1000):
+                    self.hsv((x / 1000.0) * self.max)
+                    time.sleep((period * 1.0) / 1000)
+        except KeyboardInterrupt:
+            RUNNING = False
+
+    def hue_transit(self, start=0, end=1.0, duration=4):
+        diff = end - start
+        for x in range(0, 1000):
+            self.hsv(start + (x / 1000.0) * diff)
+            time.sleep(duration / 1000.0)
+
 
